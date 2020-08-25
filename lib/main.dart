918789c48603b9b3f22cf9fab7dfce63715f5577
clement <ckoomson75@gmail.com>
2020-08-25 22:03:56 +0000

    finished with the story

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(fontFamily: 'Baloo'),
       debugShowCheckedModeBanner: false,
-      home: Books(),
+      home: StoryPage(),
     );
   }
 }
--- lib/story_logic.dart
+++ lib/story_logic.dart
@@ -59,7 +59,67 @@ class StoryLogic {
           'There are about twelve eateries at the mall, but I choose the one with the most romantic setting; daylight-lit ambiance with soft music. We take a seat. The menu is handed to us. Brined House-cut Pork Chops, Honey-glazed Baked Brie, Sundried Tomato Risotto Cakes … my temperature is beginning to rise. I begin to feel for my credit card in my pocket because my cash wouldn’t suffice. Goodness! I left it. How do I pay for these kinds of dishes? Do I tell her the truth or try to change her mind on the eatery choice?',
       firstChoice: 'Change her mind',
       secondChoice: 'Tell her the truth',
-    )
+    ),
+    Story(
+        //Scene 10
+        storyText:
+            'Quickening my pace, I stop her in her tracks, with a tap on her shoulder. “Who do you think you are? My ex is even prettier than you are... ',
+        firstChoice: 'Hi',
+        secondChoice: 'Helloo'),
+    Story(
+        //Scene 11
+        storyText:
+            '“I’ll call it unfortunate not to have spent time daydreaming about meeting you. If I had anticipated this lovely day, I would have left home with a bag full of cash to spoil you. But as it stands now, all I have on me is a few cents enough to pay for something worth not more than 50 bucks. But I’ll make it up to you soon because I envision a night out with your loveliness; not too far from today.” Her eyes widen, then she blasts away, “Did you look very carefully at me before asking me out? Do I look cheap to you? Cross my path again and you’ll really get it.”  She walks off, to my dismay. Like a boomerang, I spot an old school friend coming towards me. What do I do? What if she asks if I’m married?',
+        firstChoice: 'Dodge her',
+        secondChoice: 'Approch her'),
+    Story(
+        //Scene 12
+        storyText:
+            'I quickly look away. She is so gorgeous; toddler and baby in hand. She seems to glitter from afar; reminding me of the days I used to pursue her; those jolly high school days. My heart sinks just looking at her. I am happy she had found someone better than me, luck-wise though. Nonetheless, I’m just envisioning my life with her should she have ended up with me. Those could have been my children, my family, my ‘gorgeousness’. Stung by my bad luck, I look away, and focus on something or someone more entertaining.',
+        firstChoice: 'Leave the mall',
+        secondChoice: 'Find a hideout'),
+    Story(
+        //Scene 13
+        storyText:
+            'I excuse myself and start to approach her. I hope I’ve made the right decision. I must say, she has grown bigger than I last saw her; bigger in a very attractive way. Toddler and baby in hand, she looks towards my direction and lets a surprise draw upon her face. “Mark!” She exclaims, “You look skinnier than I last saw you.” Feigning a smile, I reply, “Do I have a choice? That’s what days without you did to my life.” “Awww,” she moans, “You know those days weren’t as serious, right? So what’s up with you now? Married?”',
+        firstChoice: 'Lie to save face',
+        secondChoice: 'Tell her the truth'),
+    Story(
+        //Scene 14
+        storyText:
+            'Rubbing my temples, I lift my head, look her straight in the eye and say, “I’d rather have you than anybody else. And since you’re already taken, I’m managing life without you.” A hush interrupts our stare. A solemn remorsefulness strikes the atmosphere as she looks back into my eyes. She looks like she would turn back the hands of time if she could, at this moment. “Mummy, I’m hungry,” the toddler calls out, breaking up our little nostalgic vibe. “Let me leave you to attend to your kids, Trisha. We’ll talk some other time.”',
+        firstChoice: 'Take her contact',
+        secondChoice: 'Don\'t bother'),
+    Story(
+        //Scene 15
+        storyText:
+            '“For a few years, until my wife cheated on me, and walked out of the marriage. So I’m back to square one; not hoping someone else’s marriage breaks up, but hoping that I find love again,”I say, almost wincing into her eyes. She gives me a sympathizing expression. “So sorry about that, Mark. So what was her name?” “Thelma,” I reply. “Is it the one who aborted in high school?” My eyes fly open. “Oh, no. I met this one at the tertiary le-.” Her phone rings before I can finish. “My husband is in to pick us up. Guess we’ll talk some other time.” “Sure.”',
+        firstChoice: 'Take her contact',
+        secondChoice: 'Don\'t bother'),
+    Story(
+        //Scene 16
+        storyText:
+            'Confused by the experience, I walked out of the mall. The afternoon breeze embraces me, sending light chills down my spine. “Why do I always have to face this kind of hustle with girls? Ain’t there no lack for a brother?” Stirring up into the sky, I feel a drop of rain peck my skin. Being single at 34 is no joke. Pacing up and down the pavement, I’m shot by a familiar sound:  “Mark!” I freeze. Who could that be? I’m so sure I know that voice. But what if it’s some other person on their way to embarrass me again? Do I turn or ignore it?',
+        firstChoice: 'Turn',
+        secondChoice: 'Ignore it'),
+    Story(
+        //Scene 17
+        storyText:
+            'I take out my latest iPhone and hand it to her to dial her cell number on it. I feel all important. Taking it from my hands, she begins to type. She is taking longer than usual so she must be saving a couple of other cell numbers on the phone. Great. “There you go,” she finally says. Taking it from her, I see her turn, and without saying goodbye she walks off, to my amazement. Surprised by her actions, I take a look at my phone, and realize that she’s typed out the following in my notes app: “Men who cheapen themselves are either always dumped or never taken seriously.” I blink in disbelief, slamming my temples; a bachelor’s hustle indeed!',
+        firstChoice: 'Finish Story',
+        secondChoice: 'Restart Story'),
+    Story(
+        //Scene 18
+        storyText:
+            'Slowly turning towards the sound, my eyes meet the unexpected. Standing three feet away from me, thin legs, blue dress, and an astonished gaze render me speechless. “For a moment I saw you, and then you weren’t there anymore. Are you okay?” Beads of sweat form on my brow as I think of the answer to give. “Sorry, but I believe you met Stan, my twin brother. We are identical twins and left home in the same clothing. We do that mostly on weekends.” She quietly stares at me, and like a blow throws this in my face, “Twin my foot! You guys will continue to remain liars till you die; disgraceful unrepentant liars!” She storms away. I blink in disbelief, slamming my temples; a bachelor’s hustle indeed! ',
+        firstChoice: 'Finish Story',
+        secondChoice: 'Restart Story'),
+    Story(
+        //Scene 19
+        storyText:
+            'Walking in the opposite direction, I quickly head for an entrance. It reads, “Pablo’s Lounge.” I enter. I’m not ready for any kind of drama so I hope I’m not being followed. Walking to a couch, I order a drink. Slouching into it, I see my nightmare come true. Walking into the lounge was Anabel, a tall dark-skinned chick I used to date. She slams down beside me and begins to speak: “The last time I saw you after you broke my heart was when you were on a date with my best friend. Not just on a date but kissing the life out of her. So appalling,” she began to raise her voice. Taking the drink I ordered from the waitress, she swings it into my face. I blink in disbelief, slamming my temples; a bachelor’s hustle indeed!',
+        firstChoice: 'Finish Story',
+        secondChoice: 'Restart Story'),
   ];
 
   int _storyNumber = 0;
@@ -104,6 +164,36 @@ class StoryLogic {
         break;
       case 8:
         choiceNumber == 1 ? _storyNumber = 4 : _storyNumber = 10;
+        break;
+      case 9:
+        choiceNumber == 1 ? _storyNumber = 0 : _storyNumber = 0;
+        break;
+      case 10:
+        choiceNumber == 1 ? _storyNumber = 11 : _storyNumber = 12;
+        break;
+      case 11:
+        choiceNumber == 1 ? _storyNumber = 15 : _storyNumber = 19;
+        break;
+      case 12:
+        choiceNumber == 1 ? _storyNumber = 14 : _storyNumber = 13;
+        break;
+      case 13:
+        choiceNumber == 1 ? _storyNumber = 16 : _storyNumber = 15;
+        break;
+      case 14:
+        choiceNumber == 1 ? _storyNumber = 16 : _storyNumber = 15;
+        break;
+      case 15:
+        choiceNumber == 1 ? _storyNumber = 18 : _storyNumber = 19;
+        break;
+      case 16:
+        choiceNumber == 1 ? _storyNumber = 0 : restart();
+        break;
+      case 17:
+        choiceNumber == 1 ? _storyNumber = 0 : restart();
+        break;
+      case 18:
+        choiceNumber == 1 ? _storyNumber = 0 : restart();
         break;
       default:
         _storyNumber = 0;
